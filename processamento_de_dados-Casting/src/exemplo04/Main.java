67a12d0469c982edfaa258c261812635b0c05de5
Lucas Henrique Costa de Oliveira <lucascosta13abc@gmail.com>
2023-05-10 21:56:24 -0300

    Aprendendo processamento de dados e Casting

---
+++ processamento_de_dados-Casting/src/exemplo04/Main.java
@@ -0,0 +1,21 @@
+package exemplo04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		// Casting = conversão explícita dos valores
+		// Colocando a palavra entre '()' em frente a expressão 
+		resultado = (double) a / b;
+		
+		System.out.println(resultado);
+
+	}
+
+}
