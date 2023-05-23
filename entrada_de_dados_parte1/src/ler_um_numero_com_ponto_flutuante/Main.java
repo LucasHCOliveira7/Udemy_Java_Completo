49336a20dfe951cb19db24767a2c9a0868fdd2d3
Lucas Henrique Costa de Oliveira <lucascosta13abc@gmail.com>
2023-05-22 21:05:39 -0300

    Entradas de dados

--- entrada_de_dados_parte1/src/ler_um_numero_com_ponto_flutuante/Main.java
+++ entrada_de_dados_parte1/src/ler_um_numero_com_ponto_flutuante/Main.java
@@ -1,11 +1,13 @@
 package ler_um_numero_com_ponto_flutuante;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		double x;
